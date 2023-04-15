eed2e2552261f069c178af0b22f9b835097468f8
九 神 <38955847+johcue@users.noreply.github.com>
2023-04-15 16:50:55 -0500

    Update sorts_matlab.m

--- sorts_matlab.m
+++ sorts_matlab.m
@@ -3,6 +3,9 @@ clear
 clc
 
 a = readmatrix('num.csv')';
-
+b = readmatrix('num_1.csv')';
+c = randi([0 1000000],1,1e6); %gen 
+tic
+%[b_select] = sort(c);%%SelectionSort
 [b_quick] = quicksort(a);%%QuickSort
-[b_select] = sort(a);%%SelectionSort
+toc
